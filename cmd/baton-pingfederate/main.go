e103b6e35c65fb017dc5240a6544ab7ef524ab9c
Steve Gontzes <gontzess@gmail.com>
2026-01-28 11:44:07 -0500

    feat: add default capabilities option to DefineConfiguration
    Add connectorrunner.WithDefaultCapabilitiesConnectorBuilder to enable
    automatic capability detection from connector interface implementation.

--- cmd/baton-pingfederate/main.go
+++ cmd/baton-pingfederate/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/conductorone/baton-pingfed/pkg/connector"
 	configSchema "github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
@@ -24,6 +25,7 @@ func main() {
 		"baton-pingfederate",
 		getConnector,
 		config.Configuration,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Connector{}),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
